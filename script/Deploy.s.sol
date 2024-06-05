099e1fa31ab8c3a57b52469f2da012f97b54167e
dan13ram <dan13ram@gmail.com>
2024-06-05 16:53:40 +0530

    mailbox deployable

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,46 +1,40 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-import {CREATE3Script} from "./base/CREATE3Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 import {console2} from "forge-std/console2.sol";
 
 import {PausableIsm} from "src/hyperlane/PausableIsm.sol";
+import {Mailbox} from "src/hyperlane/Mailbox.sol";
 
-contract DeployScript is CREATE3Script {
-    constructor() CREATE3Script(vm.envString("VERSION")) {}
+contract DeployScript is Script {
+    constructor() {}
 
     address internal _deployer;
+    PausableIsm internal _ism;
+    Mailbox internal _mailbox;
+    uint32 internal _chainId;
+    bytes32 internal constant _salt = bytes32(keccak256(abi.encode("WPOKT")));
 
-    function deployPausableIsm() external {
+    function run() external {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         _deployer = vm.addr(deployerPrivateKey);
+        _chainId = uint32(block.chainid);
 
-        console2.log("Deploying PausableIsm...");
         console2.log("Deployer: ", _deployer);
+        console2.log("Chain ID: ", _chainId);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        /* Example CREATE3 deployment
-        uint256 param = 123;
-        c = Contract(
-            create3.deploy(
-                getCreate3ContractSalt("Contract"), bytes.concat(type(Contract).creationCode, abi.encode(param))
-            )
-        ); 
-        */
+        console2.log("Deploying PausableIsm...");
+        _ism = new PausableIsm{salt: _salt}(_deployer);
+        console2.log("Deployed PausableIsm at: ", address(_ism));
 
-        bytes32 salt = getCreate3ContractSalt("PausableIsm");
-
-        address predeterminedISM = getCreate3Contract("PausableIsm");
-
-        console2.log("PredeterminedISM: ", predeterminedISM);
-
-        bytes memory creationCode = bytes.concat(type(PausableIsm).creationCode, abi.encode(_deployer));
-
-        address ism = create3.deploy(salt, creationCode);
-
-        console2.log("ISM: ", ism);
+        console2.log("Deploying Mailbox...");
+        _mailbox = new Mailbox{salt: _salt}(_chainId);
+        _mailbox.initialize(_deployer, address(_ism));
+        console2.log("Deployed Mailbox at: ", address(_mailbox));
 
         vm.stopBroadcast();
     }
--- src/hyperlane/Mailbox.sol
+++ src/hyperlane/Mailbox.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.20;
 
 // ============ Internal Imports ============
 import {Versioned} from "@hyperlane/upgrade/Versioned.sol";
