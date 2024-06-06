bddeb9305e2eaf48de24355f1efcb539d55c5d9d
dan13ram <dan13ram@gmail.com>
2024-06-06 12:25:18 +0530

    script for deploying contracts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,28 +12,34 @@ contract DeployScript is Script {
     constructor() {}
 
     address internal _deployer;
+    address internal _owner;
     PausableIsm internal _ism;
     Mailbox internal _mailbox;
     uint32 internal _chainId;
-    bytes32 internal constant _salt = bytes32(keccak256(abi.encode("WPOKT")));
+    uint256 internal _nonce;
 
     function run() external {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         _deployer = vm.addr(deployerPrivateKey);
         _chainId = uint32(block.chainid);
+        _owner = _deployer;
+        _nonce = vm.getNonce(_deployer);
 
         console2.log("Deployer: ", _deployer);
         console2.log("Chain ID: ", _chainId);
+        console2.log("Owner: ", _owner);
 
         vm.startBroadcast(deployerPrivateKey);
 
         console2.log("Deploying PausableIsm...");
-        _ism = new PausableIsm{salt: _salt}(_deployer);
+        address _predicted = vm.computeCreateAddress(_deployer, ++_nonce);
+        _ism = new PausableIsm(_owner);
         console2.log("Deployed PausableIsm at: ", address(_ism));
+        assert(address(_ism) == _predicted, "PausableIsm address mismatch");
 
         console2.log("Deploying Mailbox...");
-        _mailbox = new Mailbox{salt: _salt}(_chainId);
-        _mailbox.initialize(_deployer, address(_ism));
+        _mailbox = new Mailbox(_chainId);
+        _mailbox.initialize(_owner, address(_ism));
         console2.log("Deployed Mailbox at: ", address(_mailbox));
 
         vm.stopBroadcast();
