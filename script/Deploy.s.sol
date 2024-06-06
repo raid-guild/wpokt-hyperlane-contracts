4d185be6364d3953d55e9b9c39fd9393f49d705d
dan13ram <dan13ram@gmail.com>
2024-06-06 12:39:53 +0530

    completed script for deploying full setup

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,6 +7,8 @@ import {console2} from "forge-std/console2.sol";
 
 import {PausableIsm} from "src/hyperlane/PausableIsm.sol";
 import {Mailbox} from "src/hyperlane/Mailbox.sol";
+import {OmniToken} from "src/OmniToken.sol";
+import {wPOKTMintController} from "src/wPOKTMintController.sol";
 
 contract DeployScript is Script {
     constructor() {}
@@ -15,8 +17,15 @@ contract DeployScript is Script {
     address internal _owner;
     PausableIsm internal _ism;
     Mailbox internal _mailbox;
+    OmniToken internal _token;
+    wPOKTMintController internal _mintController;
     uint32 internal _chainId;
     uint256 internal _nonce;
+
+    uint256 internal constant _mintLimit = 10 ** 18;
+    uint256 internal constant _mintPerSecond = 10 ** 16;
+
+
 
     function run() external {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
@@ -31,16 +40,34 @@ contract DeployScript is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        console2.log("Deploying PausableIsm...");
-        address _predicted = vm.computeCreateAddress(_deployer, ++_nonce);
+        address _predictedIsm = vm.computeCreateAddress(_deployer, _nonce++);
+        console2.log("Deploying PausableIsm at: ", _predictedIsm);
         _ism = new PausableIsm(_owner);
         console2.log("Deployed PausableIsm at: ", address(_ism));
-        assert(address(_ism) == _predicted, "PausableIsm address mismatch");
+        assert(address(_ism) == _predictedIsm);
 
-        console2.log("Deploying Mailbox...");
+        address _predictedMailbox = vm.computeCreateAddress(_deployer, _nonce++);
+        console2.log("Deploying Mailbox at: ", _predictedMailbox);
         _mailbox = new Mailbox(_chainId);
         _mailbox.initialize(_owner, address(_ism));
         console2.log("Deployed Mailbox at: ", address(_mailbox));
+        assert(address(_mailbox) == _predictedMailbox);
+
+        _nonce+=1;
+
+        address _predictedToken = vm.computeCreateAddress(_deployer, _nonce++);
+        address _predictedMintController = vm.computeCreateAddress(_deployer, _nonce++);
+
+        console2.log("Deploying Token at: ", _predictedToken);
+        _token = new OmniToken(_owner, address(_predictedMintController), _owner, "Wrapped POKT", "wPOKT");
+        console2.log("Deployed Token at: ", address(_token));
+        assert(address(_token) == _predictedToken);
+
+        console2.log("Deploying MintController at: ", _predictedMintController);
+        _mintController = new wPOKTMintController(address(_mailbox), address(_token), address(_ism), _owner, _mintLimit, _mintPerSecond);
+        console2.log("Deployed MintController at: ", address(_mintController));
+        assert(address(_mintController) == _predictedMintController);
+
 
         vm.stopBroadcast();
     }
