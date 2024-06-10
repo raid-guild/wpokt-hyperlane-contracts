69d33c23124c70b453d250f2c561af2d71c6a21e
dan13ram <dan13ram@gmail.com>
2024-06-10 09:04:14 +0530

    added docker files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -m
 
-anvil --mnemonic 'test test test test test test test test test test test junk' --host 0.0.0.0 --block-time 1 &
+anvil --mnemonic 'test test test test test test test test test test test junk' --host 0.0.0.0 --block-time 1 --chain-id ${CHAIN_ID} --port ${PORT} &
 
 sleep 5
 
-forge script /app/script/Deploy.s.sol:Deploy --rpc-url "http://127.0.0.1:8545" --private-key "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" --broadcast
+forge script DeployScript --rpc-url "http://127.0.0.1:${PORT}" --broadcast
 
 fg %1
