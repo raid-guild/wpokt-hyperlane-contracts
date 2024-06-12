30bc425597306ae3452a6cc8ea48c3541900ba39
dan13ram <dan13ram@gmail.com>
2024-06-12 19:02:36 +0530

    running anvil in silent

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -m
 
-anvil --mnemonic 'test test test test test test test test test test test junk' --host 0.0.0.0 --block-time 1 --chain-id ${CHAIN_ID} --port ${PORT} &
+anvil --mnemonic 'test test test test test test test test test test test junk' --host 0.0.0.0 --block-time 1 --chain-id ${CHAIN_ID} --port ${PORT} --silent &
 
 sleep 5
 
-forge script DeployScript --rpc-url "http://127.0.0.1:${PORT}" --broadcast
+forge script DeployScript --rpc-url "http://127.0.0.1:${PORT}" --broadcast --silent
 
 fg %1
